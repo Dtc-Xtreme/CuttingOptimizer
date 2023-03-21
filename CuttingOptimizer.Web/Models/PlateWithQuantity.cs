f6fd57cc1ede0098223d39782d9e05c592476c7a
dtc-xtreme <dtc-xtreme@hotmail.com>
2023-03-21 14:45:02 +0100

    Calculator input page

---
+++ CuttingOptimizer.Web/Models/PlateWithQuantity.cs
@@ -0,0 +1,14 @@
+﻿using CuttingOptimizer.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CuttingOptimizer.Web.Models
+{
+    public class PlateWithQuantity
+    {
+        [Required]
+        [Range(1, 10)]
+        public int Quantity { get; set; }
+
+        public Plate Plate { get; set; }
+    }
+}
---
+++ CuttingOptimizer.Web/Models/ProductWithQuantity.cs
@@ -0,0 +1,14 @@
+﻿using CuttingOptimizer.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CuttingOptimizer.Web.Models
+{
+    public class ProductWithQuantity
+    {
+        [Required]
+        [Range(1, 10)]
+        public int Quantity { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
