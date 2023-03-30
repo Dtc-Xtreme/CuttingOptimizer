23cd0b23c44b6088fb36d7ec31dd219794fba4f7
Steven Kazmierczak <steven.kazmierczak@benvitec.be>
2023-03-30 14:44:09 +0200

    test

---
+++ CuttingOptimizer.AppLogic/Models/VerticalResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuttingOptimizer.AppLogic.Models
+{
+    internal class VerticalResult
+    {
+        public VerticalResult()
+        {
+            
+        }
+
+        public VerticalResult(int quantity, int rest)
+        {
+            Quantity = quantity;
+            Rest = rest;
+        }
+
+        public int Quantity { get; set; }
+        public int Rest { get; set; }
+    }
+}
