dadc31ce16743d80ef83c687de2d171d90eb800e
Steven Kazmierczak <steven.kazmierczak@benvitec.be>
2023-04-17 15:01:33 +0200

    Validation fails

---
+++ CuttingOptimizer.Blazor/Models/CalculatorFormModel.cs
@@ -0,0 +1,18 @@
+﻿using CuttingOptimizer.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CuttingOptimizer.Blazor.Models
+{
+    public class CalculatorFormModel
+    {
+        public Saw Saw { get; set; }
+        public List<Plate> Plates { get; set; }
+        public List<Product> Products { get; set; }
+
+        public CalculatorFormModel() {
+            Saw = new Saw();
+            Plates = new List<Plate>();
+            Products = new List<Product>();
+        }
+    }
+}
