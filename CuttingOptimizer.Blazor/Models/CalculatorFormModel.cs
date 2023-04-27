0bf40c08aab9915878521dcb224e7177ac1af1b3
Steven Kazmierczak <steven.kazmierczak@benvitec.be>
2023-04-27 11:21:44 +0200

    Rework with settings

--- CuttingOptimizer.Blazor/Models/CalculatorFormModel.cs
+++ CuttingOptimizer.Blazor/Models/CalculatorFormModel.cs
@@ -6,15 +6,11 @@ namespace CuttingOptimizer.Blazor.Models
     public class CalculatorFormModel
     {
         [ValidateComplexType]
-        public Saw Saw { get; set; }
-
-        [ValidateComplexType]
         public List<Plate> Plates { get; set; }
         [ValidateComplexType]
         public List<Product> Products { get; set; }
 
         public CalculatorFormModel() {
-            Saw = new Saw();
             Plates = new List<Plate>();
             Products = new List<Product>();
         }
