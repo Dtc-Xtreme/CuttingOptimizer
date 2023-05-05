866fc924bda411efbbdb0538c8de043f9c7cb2ca
Steven Kazmierczak <steven.kazmierczak@benvitec.be>
2023-05-05 16:24:56 +0200

    Layout rework new bootstrap

--- CuttingOptimizer.Blazor/Models/CalculatorFormModel.cs
+++ CuttingOptimizer.Blazor/Models/CalculatorFormModel.cs
@@ -6,11 +6,15 @@ namespace CuttingOptimizer.Blazor.Models
     public class CalculatorFormModel
     {
         [ValidateComplexType]
+        public Saw Saw { get; set; }
+
+        [ValidateComplexType]
         public List<Plate> Plates { get; set; }
         [ValidateComplexType]
         public List<Product> Products { get; set; }
 
         public CalculatorFormModel() {
+            Saw = new Saw();
             Plates = new List<Plate>();
             Products = new List<Product>();
         }
