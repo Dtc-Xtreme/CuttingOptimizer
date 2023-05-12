f4996440f72e2bb8cb5a6b685d8e4d93ef5dec23
Steven Kazmierczak <steven.kazmierczak@benvitec.be>
2023-05-12 15:24:22 +0200

    cleanup

--- CuttingOptimizer.Api/Program.cs
+++ CuttingOptimizer.Api/Program.cs
@@ -30,21 +30,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//app.UseExceptionHandler(
-//    options =>
-//    {
-//        options.Run(
-//            async context =>
-//            {
-//                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-//                var ex = context.Features.Get<IExceptionHandlerFeature>();
-//                if (ex != null)
-//                {
-//                    await context.Response.WriteAsync(ex.Error.Message);
-//                }
-//            });
-//    });
-
 //app.UseCors(x => x
 //            .SetIsOriginAllowed(origin => true)
 //            .AllowAnyMethod()
